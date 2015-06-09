5a78b5978ef7be14aa74f1e947f6cb0df3cb3816
David Senior <uglyducklins@hotmail.com>
2015-06-08 22:02:47 -0600

    Added devise funcionality

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+# Direct to local host
+config.action_mailer.default_url_options = { :host => '
+ds-pin.herokuapp.com' }
 end
